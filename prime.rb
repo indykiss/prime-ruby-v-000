cb0c749df0967237f23941bd17e70e0c52381fde
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-08-08 16:37:24 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,9 +5,9 @@ def prime?(num)
 
 if num <= 1 
   return false
-    elsif num == 2
-  
-#    elsif   (2..n/2).none? { |i| n % i == 0}
+    elsif num == 2 
+      return true 
+      else (2..n/2).none? { |i| n % i == 0}
 
 end 
 end 
