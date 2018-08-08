b57dc2c4926fb443dbadfb64de704933379ef44b
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-08-08 16:38:49 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,7 +7,7 @@ if num <= 1
   return false
     elsif num == 2 
       return true 
-      else (2..num/2).none? { |i| n % i == 0}
+      else (2..num/2).none? { |i| num % i == 0}
         return true
 end 
 end 
