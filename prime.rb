c85e53651d9788585631702cd6b7685bf213279f
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-08-08 16:41:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,11 +3,15 @@
 
 def prime?(num)
 
-if num <= 1 || num == 4
-  return false
-    elsif num == 2 
-      return true 
-      else (2..num/2).none? { |i| num % i == 0}
-        return true
+return false if n < 2
+      (2..Math.sqrt(n)).none? {|num| length % num == 0}
+
+
+#if num <= 1 || num == 4
+ # return false
+  #  elsif num == 2 
+   #   return true 
+    #  else (2..num/2).none? { |i| num % i == 0}
+     #   return true
 end 
 end 
